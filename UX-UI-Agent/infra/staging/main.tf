9cafd829c6c53194e16b46f1415717712032baab
yassine <yassinebnyahia@gmail.com>
2026-06-18 11:51:17 +0100

    organize files

--- UX-UI-Agent/infra/staging/main.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  name_prefix        = "${var.app_name}-${var.environment}"
-  secret_name_prefix = "${var.app_name}/${var.environment}"
-  allowed_origin     = var.app_domain != "" ? format("%s://%s", var.certificate_arn != "" ? "https" : "http", var.app_domain) : ""
-}
